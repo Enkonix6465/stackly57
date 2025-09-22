b2cd2ae6e28f1ff1224edd45d91e318e17c7c72e
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-09-22 16:47:10 +0530

    feat: update image sources and enhance footer with new social media icons

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "randomuser.me",
-      },
-      {
-        protocol: "https",
         hostname: "i.pinimg.com",
       },
     ],
